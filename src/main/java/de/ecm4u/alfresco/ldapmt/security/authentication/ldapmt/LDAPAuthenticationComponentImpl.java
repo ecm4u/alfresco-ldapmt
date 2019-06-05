2e7cab70724e157dc3c6b3f7971eb4d0b2bcec0f
Lutz Horn <lutz.horn@ecm4u.de>
2019-06-05 14:32:47 +0200

    refs #7120: Übernahme der früheren Anpassungen des LDAP-Authentication-Subsystems

--- src/main/java/de/ecm4u/alfresco/ldapmt/security/authentication/ldapmt/LDAPAuthenticationComponentImpl.java
+++ src/main/java/de/ecm4u/alfresco/ldapmt/security/authentication/ldapmt/LDAPAuthenticationComponentImpl.java
@@ -30,11 +30,19 @@ import javax.naming.directory.InitialDirContext;
 
 import org.alfresco.repo.management.subsystems.ActivateableBean;
 import org.alfresco.repo.security.authentication.AbstractAuthenticationComponent;
+import org.alfresco.repo.security.authentication.AuthenticationContext;
 import org.alfresco.repo.security.authentication.AuthenticationDiagnostic;
 import org.alfresco.repo.security.authentication.AuthenticationException;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
 import org.alfresco.repo.security.authentication.ldap.LDAPInitialDirContextFactory;
+import org.alfresco.repo.security.sync.UserRegistrySynchronizer;
 import org.alfresco.repo.security.sync.ldap.LDAPNameResolver;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.alfresco.service.cmr.security.PersonService;
+import org.alfresco.service.transaction.TransactionService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
@@ -46,6 +54,18 @@ import org.springframework.beans.factory.InitializingBean;
 public class LDAPAuthenticationComponentImpl extends AbstractAuthenticationComponent implements InitializingBean,
         ActivateableBean
 {
+	    
+    // ldapmt PATCH START
+    private final Log logger = LogFactory.getLog(getClass());
+
+    // Since these services are private in the extended class and we want to use
+    // them in the pachted code, we must make them members here.
+    private UserRegistrySynchronizer userRegistrySynchronizer;    
+    private AuthenticationContext authenticationContext;
+    private TransactionService transactionService;
+    private PersonService personService;
+    // ldapmt PATCH END
+	
     private boolean escapeCommasInBind = false;
     
     private boolean escapeCommasInUid = false;
@@ -118,7 +138,30 @@ public class LDAPAuthenticationComponentImpl extends AbstractAuthenticationCompo
      * Implement the authentication method
      */
     protected void authenticateImpl(String userName, char[] password) throws AuthenticationException
-    {
+    {       
+        // ldapmt PATCH START
+        if (logger.isDebugEnabled()) {
+            logger.debug("userName=" + userName);
+        }
+        if (!userName.contains("@")) {
+            throw new AuthenticationException("no tenant in " + userName);
+        }
+
+        String tenant;
+        String[] userNameParts = userName.split("@");
+        if (userNameParts.length == 3) {
+            userName = userNameParts[0] + "@" + userNameParts[1];
+            tenant = userNameParts[2];
+        } else {
+            userName = userNameParts[0];
+            tenant = userNameParts[1];
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("userName: " + userName);
+            logger.debug("tenant: " + tenant);
+        }
+        // ldapmt PATCH END
+    	
         if (logger.isTraceEnabled())
         {
             logger.trace("Authentication for user: " + AuthenticationUtil.maskUsername(userName));
@@ -158,6 +201,11 @@ public class LDAPAuthenticationComponentImpl extends AbstractAuthenticationCompo
         try
         {
             ctx = ldapInitialContextFactory.getInitialDirContext(userDN, new String(password), diagnostic);
+            
+            // ldapmt PATCH START
+            handleUser(userName, tenant);
+            userName = userName + "@" + tenant;
+            // ldapmt PATCH END            
 
             // Authentication has been successful.
             // Set the current user, they are now authenticated.
@@ -231,4 +279,60 @@ public class LDAPAuthenticationComponentImpl extends AbstractAuthenticationCompo
     {
         return id;
     }
+    
+	// ldapmt PATCH START
+	private void handleUser(final String userName, final String tenant) {
+
+		transactionService.getRetryingTransactionHelper()
+				.doInTransaction(new RetryingTransactionHelper.RetryingTransactionCallback<Void>() {
+
+					@Override
+					public Void execute() throws Throwable {
+						AuthenticationUtil.runAs(new RunAsWork<Void>() {
+
+							@Override
+							public Void doWork() throws Exception {
+
+								if (personService.personExists(userName + "@" + tenant)) {
+									return null;
+								}
+
+								if (!userRegistrySynchronizer.createMissingPerson(userName + "@" + tenant)) {
+									throw new AuthenticationException("User \"" + userName
+											+ "\" does not exist and could not be created in Alfresco");
+								}
+
+								return null;
+							}
+						}, authenticationContext.getSystemUserName(tenant));
+						return null;
+					}
+				}, false, true);
+
+	}
+	
+	@Override
+	public void setAuthenticationContext(AuthenticationContext authenticationContext) {
+		super.setAuthenticationContext(authenticationContext);
+		this.authenticationContext = authenticationContext;
+	}
+
+	@Override
+	public void setUserRegistrySynchronizer(UserRegistrySynchronizer userRegistrySynchronizer) {
+		super.setUserRegistrySynchronizer(userRegistrySynchronizer);
+		this.userRegistrySynchronizer = userRegistrySynchronizer;
+	}
+
+	@Override
+	public void setPersonService(PersonService personService) {
+		super.setPersonService(personService);
+		this.personService = personService;
+	}
+
+	@Override
+	public void setTransactionService(TransactionService transactionService) {
+		super.setTransactionService(transactionService);
+		this.transactionService = transactionService;
+	}
+	// ldapmt PATCH END    
 }
